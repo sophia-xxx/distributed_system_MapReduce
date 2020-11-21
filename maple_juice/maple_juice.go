10ee5a1ed578e36ca0cde8b6f7e130037b877aea
sophia <sophia.cation@gmail.com>
2020-11-22 01:03:13 +0800

    new

--- maple_juice/maple_juice.go
+++ maple_juice/maple_juice.go
@@ -1,8 +1,12 @@
 package maple_juice
 
 import (
+	"fmt"
 	"google.golang.org/protobuf/types/known/timestamppb"
+	"mp3/file_system"
 	"mp3/net_node"
+	"net"
+	"net/rpc"
 )
 
 /*
@@ -10,25 +14,76 @@ split the whole sdfs file and generate file clips for every maple task
 */
 func splitFile(filePath string, mapleNum int) []string {
 	fileClips := make([]string, mapleNum)
-	// read lines of sdfs_src_file
+	// TODO:read lines of sdfs_src_file
 	return fileClips
+}
+
+// define server interface
+type Server struct {
+	IP   string
+	Port string
+}
+
+// features to describe maple/juice task
+type Task struct {
+	TaskNum   int
+	FileName  string
+	FilePath  string
+	FileStart int
+	FileEnd   int
+	Status    string
+	TaskType  string // "maple"/"juice"
+	ServerIp  string
+	LastTime  *timestamppb.Timestamp
+}
+
+/*
+init sever
+*/
+func (mapleServer *Server) newMapleServer() *Server {
+	server := &Server{
+		//TODO:get ip and port of this node
+	}
+	return server
 }
 
 /*
 Server run maple task on file clip
 */
-func MapleTask(fileName string, fileStart int, fileEnd int) {
+//fileName string, fileStart int, fileEnd int
+func (mapleServer *Server) MapleTask(args Task, replyKeyList *[]string) error {
 	// read file clip, same as "get" command
+	node := &net_node.Node{
+		//TODO: how to use node??
+	}
+	go file_system.GetFile(node, args.FileName, args.FilePath)
+	//TODO: notify that the file has been stored successfully
 
 	// execute maple_exe
 
 	// reply to master
 
 	// how to deal with maple_local_file??
+	return nil
 }
 
 /*
-Server run maple task on file clip
+Server start listening RPC call
+*/
+func StartRPC(mapleServer *Server) {
+	rpc.Register(mapleServer)
+	listener, _ := net.Listen("tcp", "1234")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("error...")
+		}
+		go rpc.ServeConn(conn)
+	}
+}
+
+/*
+Server run juice task on file clip
 */
 func JuiceTask(fileList []string) {
 	// loop fileList
@@ -49,18 +104,6 @@ func JuiceTask(fileList []string) {
 // master keep record of all maple/reduce tasks
 var taskMap map[string]Task
 
-// features to describe maple/juice task
-type Task struct {
-	TaskNum   int
-	FileName  string
-	FileStart int
-	FileEnd   int
-	Status    string
-	TaskType  string // "maple"/"juice"
-	ServerIp  string
-	LastTime  *timestamppb.Timestamp
-}
-
 /*
 Master init all variables
 */
@@ -80,6 +123,11 @@ func schedule(fileClips []string) {
 Master shuffle keys to generate N juice tasks
 */
 func shuffle() {
+	client, _ := rpc.Dial("tcp", address)
+	args := &Task{}
+	mapleResults := make([]string, 9)
+	callServer := client.Go("MJServer.MapleTask", args, mapleResults, nil)
+	replyCall := <-callServer.Done
 
 }
 
