c74aa9d8742d7967602afcec736e2d2067271c5e
Shen <chunhao3@fa20-cs425-g07-03.cs.illinois.edu>
2020-11-24 04:16:19 -0600

    syscall

--- mapleJuice_exe/syscall.go
+++ mapleJuice_exe/syscall.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	_, err := exec.Command("/bin/sh", "-c", "maple_vote < maple_vote_test1 >result").Output()
+	_, err := exec.Command("/bin/sh", "-c", "./maple_vote < maple_vote_test1 >result").Output()
 	if err != nil {
 		fmt.Printf("%s", err)
 	}
