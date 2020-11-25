9141503f791350359a8b4b52c1b98332b3533f9e
sophia <sophia.cation@gmail.com>
2020-11-26 02:22:56 +0800

    debug

--- config/config.go
+++ config/config.go
@@ -8,7 +8,7 @@ import (
 const (
 	CLIPPREFIX  = "sdfs_src_file_clip_"
 	FILEPREFIX  = "sdfs_intermediate_file_prefix_"
-	RPCPORT     = "1234"
+	RPCPORT     = "6006"
 	GETFILEWAIT = 4 * time.Second
 	MASTERIP    = "172.22.156.22"
 )
