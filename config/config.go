f9b7bcb94a9340583c77d905880d5dbe899277ca
sophia <sophia.cation@gmail.com>
2020-11-26 18:13:17 +0800

    fix rpc bug

--- config/config.go
+++ config/config.go
@@ -8,7 +8,7 @@ import (
 const (
 	CLIPPREFIX  = "sdfs_src_file_clip_"
 	FILEPREFIX  = "sdfs_intermediate_file_prefix_"
-	RPCPORT     = "6012"
+	RPCPORT     = "6014"
 	GETFILEWAIT = 6 * time.Second
 	MASTERIP    = "172.22.156.22"
 )
