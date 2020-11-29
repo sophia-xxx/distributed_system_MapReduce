87aa9891b36ec4879b289761fdb6b7af9818c5ab
sophia <sophia.cation@gmail.com>
2020-11-30 01:02:20 +0800

    add juice vote

---
+++ mj_exe/juice_vote.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	var canLeft string
+	var canRight string
+	var numZero int
+	var numOne int
+	for {
+		if !input.Scan() {
+			break
+		}
+		line := input.Text()
+		line = strings.TrimSpace(line)
+		str := strings.Split(line, " ")
+		canPair := str[0]
+		canTemp := strings.Split(canPair, ",")
+		canLeft = canTemp[0]
+		canRight = canTemp[1]
+		numZero = 0
+		numOne = 0
+		value := str[1]
+		if strings.Compare(value, "0") == 0 {
+			numZero++
+		}
+		if strings.Compare(value, "1") == 0 {
+			numOne++
+		}
+	}
+	if numOne > numZero {
+		fmt.Println(canLeft + "," + canRight)
+	} else {
+		fmt.Println(canRight + "," + canLeft)
+	}
+
+}
---
+++ mj_exe/juice_vote_two.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	carray := make(map[string]int)
+	max := 0
+	var maxCan string
+	input := bufio.NewScanner(os.Stdin)
+	for {
+		if !input.Scan() {
+			break
+		}
+
+		line := input.Text()
+		line = strings.TrimSpace(line)
+		// "1 A,B"
+		// "1 A,B"
+		str := strings.Split(line, " ")
+		candidates := strings.Split(str[1], ",")
+		winner := candidates[0]
+		//canRight:=candidates[1]
+		carray[winner]++
+	}
+	for can, count := range carray {
+		if count > max {
+			max = count
+			maxCan = can
+		}
+	}
+	fmt.Println(maxCan + " win!")
+
+}
