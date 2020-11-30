9e289cd854dac2fc3fc5b3cd6443350a3165b3a8
sophia <sophia.cation@gmail.com>
2020-11-30 19:15:44 +0800

    debug

--- mj_exe/juice_wordcount.go
+++ mj_exe/juice_wordcount.go
@@ -1,22 +1,30 @@
 package main
 
-//func main() {
-//	input := bufio.NewScanner(os.Stdin)
-//	var wcmap map[string]int
-//	wcmap = make(map[string]int)
-//	for {
-//		if !input.Scan() {
-//			break
-//		}
-//		line := input.Text()
-//		line = strings.TrimSpace(line)
-//		str := strings.Split(line, " ")
-//		wordkey := str[0]
-//		count, _ := strconv.Atoi(str[1])
-//		wcmap[wordkey] += count
-//
-//	}
-//	for k, v := range wcmap {
-//		fmt.Printf("%s %d\n", k, v)
-//	}
-//}
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	var wcmap map[string]int
+	wcmap = make(map[string]int)
+	for {
+		if !input.Scan() {
+			break
+		}
+		line := input.Text()
+		line = strings.TrimSpace(line)
+		str := strings.Split(line, " ")
+		wordkey := str[0]
+		count, _ := strconv.Atoi(str[1])
+		wcmap[wordkey] += count
+
+	}
+	for k, v := range wcmap {
+		fmt.Printf("%s %d\n", k, v)
+	}
+}
